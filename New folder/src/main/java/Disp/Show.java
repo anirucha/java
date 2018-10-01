650a604d3a46a07448de064983ecbd3923e19f03
Chatterjee <aniruddh.chatterjee@capgemini.com>
2018-10-01 11:32:59 +0530

    added maven

---
+++ New folder/src/main/java/Disp/Show.java
@@ -0,0 +1,12 @@
+package Disp;
+
+public class Show {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a maven build being run on jenkins");
+		
+	}
+
+}
